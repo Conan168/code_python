9ac0d170b57bedda5639d6cb97244964fe42f4ca
Conan.Yu <abc@gmail.com>
2026-09-17 10:11:48 +0800

    feature(Intermediate): first commit of Intermediate - coding a project coffee machine by OOP

---
+++ Intermediate/coffee_machine_OOP/money_machine.py
@@ -0,0 +1,45 @@
+"""
+define the payment mode
+"""
+
+class MoneyMachine:
+    """Models the payment that using coins"""
+
+    CURRENCY = "$"
+
+    COIN_VALUES = {
+        "quarters": 0.25,
+        "dimes": 0.10,
+        "nickles": 0.05,
+        "pennies": 0.01
+    }
+
+    def __init__(self):
+        self.profit = 0
+        self.money_received = 0
+
+    def report(self):
+        """Prints the current profit"""
+        print(f"Money: {self.CURRENCY}{self.profit}")
+
+    def process_coins(self):
+        """Returns the total calculated from coins inserted."""
+        for coin, value in self.COIN_VALUES.items():
+            self.money_received += int(input(f"How many {coin} (${value})?: ") or 0 ) * value
+        return self.money_received
+
+    def make_payment(self, cost):
+        """Returns True when payment is accepted, or False if insufficient."""
+        print(f"Please insert ${cost} coins.")
+        self.process_coins()
+        if self.money_received >= cost:
+            change = round(self.money_received - cost, 2)
+            print(f"Here is {self.CURRENCY}{change} in change.")
+            self.profit += cost
+            self.money_received = 0
+            return True
+
+        print("Sorry that's not enough money. Money refunded.")
+        self.money_received = 0
+        return False
+    
